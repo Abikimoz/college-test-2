2efb098240b3b690db6ff2cf4822a7d14c9779dd
Abikimoz <i.m.anisimov25@gmail.com>
2023-05-29 22:19:17 +0300

    Step 4 done

--- index.js
+++ index.js
@@ -44,6 +44,74 @@ let quantityTwoPM = 20;
 
 console.log(`2) 10 самых сильных существ и 20 вторых по силе стоят:`, quantityPM*pricePowerMaxIndex+quantityTwoPM*priceTwoPowerMaxIndex);
 
+let height = data.map((row) => Number(row.split('|').at(5)));
+// console.log(height);
+let weight = data.map((row) => Number(row.split('|').at(6)));
+// console.log(weight);
 
+let i = 0;
+let thinness = height.map((row) => {
+  // console.log(row);
+  row = row / weight[i];
+  i ++;
+  // console.log(row);
+  row = row.toFixed(2);
+  row = Number(row.split('|'));
+  return row;
+});
+
+// худые
+let thin = Math.max(...thinness);
+// толстые
+let thick = Math.min(...thinness);
+
+let thinIndex = thinness.indexOf(thin);
+let thickIndex = thinness.indexOf(thick);
+
+// console.log(thinness);
+// console.log(thinIndex);
+// console.log(thickIndex);
+
+let unit = data.map((row) => Number(row.split('|').at(4)));
+// console.log(unit);
+// console.log(price);
+
+i = 0
+let squadPrice = unit.map((row) => {
+  // console.log(row);
+  row = row * price[i];
+  i ++;
+  // console.log(row);
+  // row = row.toFixed(2);
+  // row = Number(row.split('|'));
+  return row;
+});
+
+// console.log(squadPrice);
+
+console.log(`3) Стоимость отряда самых толстых: ${squadPrice[thickIndex]} Отряда самых худых ${squadPrice[thinIndex]}`);
+
+i = 0
+let pricePower = price.map((row) => {
+  row = row / power[i];
+  i ++;
+  // console.log(row);
+  row = row.toFixed(2);
+  row = Number(row.split('|'));
+  return row;
+});
+
+let advantageous = Math.max(...pricePower);
+let notProfitable = Math.min(...pricePower);
+
+let advantageousIndex = pricePower.indexOf(advantageous);
+let notProfitableIndex = pricePower.indexOf(notProfitable);
+
+let unitName = data.map((row) => row.split('|').at(1));
+console.log(unitName);
+
+console.log(`4) Cамым невыгодным по соотношению цены и силы являются юниты ${unitName[advantageousIndex]}, a самым выгодным ${unitName[notProfitableIndex]}`);
+
+console.log(pricePower);
 
 // END
